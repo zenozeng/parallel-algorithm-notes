51dffa78315505d2d4fbe6cd8a439e5aa48cf92f
Zeno Zeng <zenoofzeng@gmail.com>
2015-06-03 20:34:57 +0800

    update prefix algorithm

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-pandoc 02.md -s --highlight-style pygments -c github.css --mathjax -o dist/02.html
+pandoc 02.md -s --highlight-style pygments -c github.css -c styles.css --mathjax -o dist/02.html
+cp *.css dist
